206103cb6fed58a1eb431eb22c8c102345768f39
katygz <katygui920@gmail.com>
2024-10-20 15:27:32 +0800

    modify content

---
+++ js/projects.js
@@ -0,0 +1,321 @@
+const projectsData = [
+    {
+        id: 'AB',
+        title: 'AB',
+        image: 'img/projects/ab.jpg',
+        carouselImages: [
+            {
+                img: 'img/projects/ab.jpg',
+                desc: '',
+            }
+        ],
+        description: 'Worked with cross-functional teams to design and run A/B tests, analyzing data from 90,189 players to produce insights that improved user engagement and retention. Using Python, built a logistic regression model to predict day 1 and day 7 retention based on historical gaming data, driving key decisions and enhancing game design to strengthen retention strategies.',
+        tags: [
+                {title: 'A/B Testing',},
+                {title: 'Python',},
+                {title: 'Logistic Regression',},
+                {title: 'Predictive Modeling',},
+                {title: 'Retention Strategy',},
+                {title: 'Cross-Functional Collaboration',},
+            ],
+    },
+    {
+        id: 'machine_learning',
+        title: 'Machine Learning',
+        image: 'img/projects/ml.png',
+        carouselImages: [
+            {
+                img: 'img/projects/ml1.jpg',
+                desc: '',
+            }
+        ],
+        description: 'Analyzed and cleaned data on Indian house rental prices using Pandas and Matplotlib, identifying key factors influencing rental rates. Developed a machine learning-based pricing model to recommend optimal prices for new listings, ensuring both profitability for the company and stability for clients.',
+        tags: [
+                {title: 'Pandas',},
+                {title: 'Matplotlib',},
+                {title: 'Machine Learning',},
+                {title: 'Pricing Model',},
+                {title: 'Data Analysis',},
+            ],
+    },
+    {
+        id: 'News',
+        title: 'News',
+        image: 'img/projects/news.jpg',
+        carouselImages: [
+            {
+                img: 'img/projects/news1.jpg',
+                desc: '',
+            }
+        ],
+        description: 'This project focuses on building a personalized news recommendation system to enhance user engagement and satisfaction. Key steps include data preprocessing to clean and prepare data, followed by exploratory data analysis (EDA) to identify patterns. Feature engineering extracts relevant characteristics to improve recommendation accuracy, while model development creates algorithms to make predictions. The model\'s performance is evaluated using metrics such as accuracy, precision, and recall, with further optimization through hyperparameter tuning and new feature incorporation. Finally, the model is deployed in a production environment and monitored over time, often using A/B testing to ensure continuous improvement.',
+        tags: [
+                {title: 'Feature Engineering',},
+                {title: 'Recommendation System Development',},
+                {title: 'Predictive Modeling',},
+                {title: 'Model Evaluation',},
+                {title: 'A/B Testing',},
+            ],
+    },
+
+
+
+    {
+        id: 'Revolutionizing',
+        title: 'Revolutionizing',
+        image: 'img/projects/revolutionizing.jpg',
+        carouselImages: [
+            {
+                img: 'img/projects/revolutionizing.jpg',
+                desc: '',
+            }
+        ],
+        description: 'Yu\'ebao invests idle funds in low-risk money market instruments, offering liquidity while managing risks from market fluctuations, credit exposure, and regulations. Using Python for data analysis, market research, policy tracking, investor sentiment, and time series analysis, it predicts fund flows to optimize investments, enhance fund management, mitigate risks, and strengthen competitive advantages.',
+        tags: [
+            {title: 'Python',},
+            {title: 'Data Analysis',},
+            {title: 'Time Series Analysis',},
+            {title: 'Fund Flow Prediction',},
+            {title: 'Risk Management',},
+        ],
+    },
+
+
+    {
+        id: 'Ui',
+        title: 'Ui',
+        image: 'img/projects/ui.jpg',
+        carouselImages: [
+            { img: 'img/projects/ui1.jpg', desc: '',},
+            { img: 'img/projects/ui2.png', desc: '',},
+        ],
+        description: 'This project analyzes user interactions with a course page to evaluate the impact of a new UI change. Key steps include calculating goal metrics for control and test groups to measure performance differences. The analysis applies **bootstrap methods** to compute confidence intervals, ensuring statistical reliability. Graphs are used to visualize the results for clearer interpretation. Finally, recommendations are provided based on the findings, with an optional **logistic regression model** applied to further assess whether the UI change should be implemented. This process emphasizes **statistical analysis, data visualization, and predictive modeling** for informed decision-making.',
+        tags: [
+                {title: 'A/B Testing',},
+                {title: 'Bootstrap Methods',},
+                {title: 'Data Visualization',},
+                {title: 'Logistic Regression',},
+                {title: 'Statistical Analysis',},
+        ],
+    },
+
+
+    {
+        id: 'Yelp',
+        title: 'Yelp',
+        image: 'img/projects/yelp.png',
+        carouselImages: [
+            {
+                img: 'img/projects/yelp1.jpg',
+                desc: '',
+            }
+        ],
+        description: 'The approach to analyzing the Yelp data involves several key steps: preprocessing the dataset, using various analysis methods (regression, classification, clustering, time series analysis), evaluating the data from different perspectives (platform, users, merchants), creating visual representations Time series analysis is particularly important, focusing on user reviews and merchant feedback over time to identify trends and changes. Tools like Python, R, and SQL are used for scripting, statistical analysis, and data querying. The project aims to provide deep insights into user behaviors and business operations on Yelp, underlining the importance of analytical skills, problem-solving abilities, and teamwork in data analysis projects',
+        tags: [
+                {title: 'Python, R, SQL',},
+                {title: 'Time Series Analysis',},
+                {title: 'Regression Analysis',},
+                {title: 'Data Preprocessing',},
+                {title: 'Data Visualization',},
+            ],
+    },
+
+
+    {
+        id: 'An_interactive',
+        title: 'An interactive',
+        image: 'img/projects/interactive.jpg',
+        carouselImages: [
+            {
+                img: 'img/projects/interactive.jpg',
+                desc: '',
+            }
+        ],
+        description: 'Released the inaugural application, innovatively designed with R and MySQL Server and showcased through R Shiny, spotlighting a selection of the finest restaurants voted by food aficionados worldwide! This app includes an interactive map to effortlessly navigate the gourmet landscape.',
+        tags: [
+                {title: 'R',},
+                {title: 'MySQL Server',},
+                {title: 'R Shiny',},
+                {title: 'Application Development',},
+                {title: 'Interactive Mapping',},
+                {title: 'Data Visualization',},
+            ],
+    },
+
+
+    {
+        id: 'business_dognition',
+        title: 'Business Process Change Recommendations for Dognition',
+        image: 'img/projects/dognition.png',
+        carouselImages: [
+            {
+                img: 'img/projects/dognition.png',
+                desc: '',
+            }
+        ],
+        description: 'Presented a data-driven business process change proposal to Dognition company management, focused on increasing the number of tests users complete on their website. Conducted data analysis using Tableau, creating multiple dashboards to visualize insights from Dognition data, supporting the proposal with clear, actionable recommendations.',
+        tags: [
+                {title: 'Tableau',},
+                {title: 'Dashboard Development',},
+                {title: 'Data Analysis',},
+                {title: 'Business Process Improvement',},
+                {title: 'Data-Driven Decision Making',},
+            ],
+    },
+
+
+    {
+        id: 'Increasing',
+        title: 'Increasing',
+        image: 'img/projects/increasing.jpg',
+        carouselImages: [
+            {
+                img: 'img/projects/increasing.jpg',
+                desc: '',
+            }
+        ],
+        description: 'Conducted a capstone project for Watershed Property Management, Inc., using data analysis to recommend strategies for improving profits. Leveraged Excel and MySQL to identify the best opportunities for revenue growth and profit maximization while addressing potential risks. Developed Tableau dashboards to visualize key insights, ensuring data-driven recommendations were clear and actionable.',
+        tags: [
+                {title: 'Excel',},
+                {title: 'MySql',},
+                {title: 'Tableau',},
+                {title: 'Profit Maximization',},
+                {title: 'Risk Management',},
+            ],
+    },
+
+
+
+
+
+
+
+
+
+    // if not enough for 3 columns in one row, put some empty placeholder div here to make it up to 3
+   // {
+   //      id: '',
+   //  },
+   //  {
+   //      id: '',
+   //  },
+
+
+
+
+];
+
+
+function chunkArray(array, chunkSize) {
+    let result = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+        result.push(array.slice(i, i + chunkSize));
+    }
+    return result;
+}
+
+function createTileModal (pjItem) {
+
+    const carouselInfo = [];
+    pjItem.carouselImages?.map(item => {
+        const scrollItem = `
+            <figure class="image is-4by3 carousel-cell">
+                    <img src="${item.img}">
+            </figure>
+        `;
+        carouselInfo.push(scrollItem);
+    });
+
+    const tagsInfo = [];
+    pjItem.tags?.map(item => {
+        const tagItem = `
+            <span class="tag">${item.title}</span>
+        `;
+        tagsInfo.push(tagItem);
+    });
+
+    let tileTpl = '';
+    let modalTpl = '';
+
+    if (pjItem.id) {
+        tileTpl = `
+          <div class="tile is-parent">  
+            <article class="tile is-child notification">
+                    <p class="title" title="${pjItem.title}">${pjItem.title}</p>
+                    <figure class="image is-3by2">
+                      <img class="modal-trigger" data-target="${pjItem.id}" src="${pjItem.image}">
+                    </figure>
+            </article>
+          </div>
+        `;
+        modalTpl = `    
+            <div id="${pjItem.id}" class="modal">
+                <div class="modal-background"></div>
+                <div class="modal-card">
+                  <header class="modal-card-head">
+                    <p class="modal-card-title">${pjItem.title}</p>
+                    <button class="delete" aria-label="close"></button>
+                  </header>
+                  <section class="modal-card-body">
+                    <div id="${pjItem.id}-carousel" class="carousel">
+                      ${carouselInfo.join('\n')}
+                    </div>
+                    <div class="content">
+                      <p>${pjItem.description}</p>
+                      <div class="tags">
+                        ${tagsInfo.join('\n')}
+                      </div>
+                    </div>
+                  </section>
+                  <footer class="modal-card-foot">
+                    <button class="button is-success">Close</button>
+                  </footer>
+                </div>
+              </div>
+        `;
+    } else {
+        tileTpl = `
+          <div class="tile is-parent">  
+            <!--placeholder--> 
+          </div>
+        `;
+    }
+
+
+    return {tileTpl, modalTpl};
+}
+
+function displayProjects () {
+
+    const groupDivs = [];
+    const modalDivs = [];
+    chunkArray(projectsData, 3).map(groupItems => {
+
+        const tileItems = [];
+        groupItems.map(item => {
+            const {tileTpl, modalTpl} = createTileModal(item);
+
+            tileItems.push(tileTpl);
+            modalDivs.push(modalTpl);
+        });
+
+        const groupTpl = `
+             <div class="tile is-ancestor">
+                <div class="tile is-vertical is-12">
+                  <div class="tile">
+                    ${tileItems.join('\n')}
+                  </div>
+                </div>
+             </div>
+        `;
+        groupDivs.push(groupTpl);
+
+    });
+    // const {tileTpl, modalTpl} = createTileModal(projectsData[0]);
+    const projectTilesWrapper = document.getElementById('project_tiles_wrapper');
+    const projectModalsWrapper = document.getElementById('project_modals_wrapper');
+    projectTilesWrapper.innerHTML = groupDivs.join('\n');
+    projectModalsWrapper.innerHTML = modalDivs.join('\n');
+}
+
+displayProjects ();
