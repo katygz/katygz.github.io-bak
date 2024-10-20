d103c11c331f570e3acd20016af86f18d9396467
Zhi（Katy）Gui <137499345+katygz@users.noreply.github.com>
2024-10-20 17:32:41 -0500

    projects.js

--- js/projects.js
+++ js/projects.js
@@ -130,7 +130,7 @@ const projectsData = [
     {
         id: 'An_interactive',
         // title: 'An Interactive R Shiny App Mapping the Worlds Top-Rated Restaurants',
-        title: 'Dallas Home Sales analysis by R shine',
+        title: 'Dallas Real EstateInsights-Powered by R Shiny',
         detailLink: 'https://github.com/katygz/An-Interactive-R-Shiny-App-Mapping-the-Worlds-Top-Rated-Restaurants',
         image: 'img/projects/interactive.png',
         carouselImages: [
